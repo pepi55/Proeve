23ef3efc5cb8e3d43e461d0b4a2e4da0e99ac680
pepi <pepi55@live.com>
2016-04-01 14:17:28 +0200

    Attempt at drawing picture with HTML5.

--- HTML5/main.js
+++ HTML5/main.js
@@ -1,2 +1,14 @@
 // Initialize Phaser
-var game = new Phaser.Game(800, 600);
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+
+// Main game state.
+function preload() {
+	game.load.image('character', 'assets/character.jpg');
+}
+
+function create() {
+	game.stage.backgroundColor = '#CCCCCC';
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	this.character = game.add.sprite(100, 100, 'character');
+}
