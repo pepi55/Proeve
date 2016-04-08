86e3a4d34482c34823002bd03c7d0b09b27789cc
Pepi Dimi <pepi55@live.com>
2016-04-08 15:58:33 +0000

    changed render mode to canvas this greatly improved preformance on mobile devices

--- HTML5/main.js
+++ HTML5/main.js
@@ -1,5 +1,5 @@
 // Create game with phaser.
-var game = new Phaser.Game(1024, 1366);
+var game = new Phaser.Game(1024, 1366,Phaser.CANVAS);
 
 // Add game states.
 game.state.add('mainMenu', this.mainMenuState);
