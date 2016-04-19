f7c2b7117f72939d8012b47ecb1be3f2b168dfea
Pepi Dimi <pepi55@live.com>
2016-04-19 14:18:25 +0000

    highscoremenuscript added

---
+++ HTML5/highscoreMenu.js
@@ -0,0 +1,20 @@
+// highscore state.
+var highscoreList = new Array(10);
+
+var highscoreState = {
+  // Custom "variables".
+
+  // Phaser functions.
+  preload: function() {
+
+  console.log(highscoreList[0]);
+  },
+
+  create: function() {
+
+  },
+
+  update: function() {
+
+  },
+};
