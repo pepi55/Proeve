477f36d9d0aa93382755da1d8c9323a228f6b146
Nolimet <nolimet@stamdata.nl>
2016-04-20 10:16:29 +0200

    tweaked the colour of the letters a lil bit making it a bit easier to read

--- Unity/Assets/Scripts/UI/HighScoreMenu.cs
+++ Unity/Assets/Scripts/UI/HighScoreMenu.cs
@@ -14,13 +14,18 @@ public class HighScoreMenu : MonoBehaviour
     void Start()
     {
         GameObject g;
+        HighScoreDisplayObject obj;
         bool dark = false;
         for (int i = 0; i < SaveManager.savaData.highScores.Length; i++)
         {
             g = Instantiate(HighScoreObjectTemplate) as GameObject;
             g.SetActive(true);
             g.transform.SetParent(scoreBoard,false);
-            g.GetComponent<HighScoreDisplayObject>().SetValues(SaveManager.savaData.highScores[i].name, SaveManager.savaData.highScores[i].score.ToString(), dark ? Color.gray : Color.white);
+            obj = g.GetComponent<HighScoreDisplayObject>();
+
+            obj.SetValues(SaveManager.savaData.highScores[i].name, SaveManager.savaData.highScores[i].score.ToString(), dark ? Color.gray : Color.white);
+            obj.Name.color = dark ? (Color.grey + (Color.white / 4f)) : Color.black;
+            obj.Score.color = dark ? (Color.grey + (Color.white / 4f)) : Color.black;
             dark = !dark;
         }
 
