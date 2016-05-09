2be968919386f5596a647394d4616b6ad57fd65d
Pepi Dimi <pepi55@live.com>
2016-05-09 19:57:20 +0000

    options couple of resolutions added

--- HTML5/main.js
+++ HTML5/main.js
@@ -1,5 +1,5 @@
 // Create game with phaser.
-var game = new Phaser.Game(1024, 1024, Phaser.CANVAS);
+var game = new Phaser.Game(1920, 1024, Phaser.CANVAS);
 
 // Add game states.
 game.state.add('mainMenu', this.mainMenuState);
--- HTML5/optionsMenu.js
+++ HTML5/optionsMenu.js
@@ -2,18 +2,29 @@
 
 
 var optionsState = {
-  // Custom "variables".
-
-  // Phaser functions.
-  preload: function() {
-
-  },
-
   create: function() {
+    var reso2048x1536;
+		reso2048x1536 = game.add.button(game.world.centerX, game.world.centerY, 'button', function() {
+		this.scale.setGameSize(2048, 1536);
+		}, this);
+		reso2048x1536.anchor.setTo(0.5, 0.5);
 
-  },
+		var reso1920x1080;
+		reso1920x1080 = game.add.button(game.world.centerX, game.world.centerY + 100, 'button', function() {
+				this.scale.setGameSize(1920, 1080);
+		}, this);
+		reso1920x1080.anchor.setTo(0.5, 0.5);
 
-  update: function() {
+		var reso1366x768;
+		reso1366x768 = game.add.button(game.world.centerX, game.world.centerY + 200, 'button', function() {
+				this.scale.setGameSize(1366, 768);
+		}, this);
+		reso1366x768.anchor.setTo(0.5, 0.5);
 
+			var backButton;
+		backButton = game.add.button(50, 50, 'button', function() {
+		game.state.start('mainMenu');
+		}, this);
+		backButton.anchor.setTo(0.5, 0.5);
   },
 };
