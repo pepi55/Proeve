39b8fd9ce1a20cbd4f2f00f8808a3bd1ce4f2090
Nolimet <nolimet@stamdata.nl>
2016-05-10 12:38:04 +0200

    added comments and cleaned code a lil

--- Unity/Assets/Scripts/UI/HighScore/HighScoreDisplayObject.cs
+++ Unity/Assets/Scripts/UI/HighScore/HighScoreDisplayObject.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// A display object for in the highscore screen
+/// </summary>
 public class HighScoreDisplayObject : MonoBehaviour
 {
     [SerializeField]
-    UnityEngine.UI.Text score = null, _name = null;
+    UnityEngine.UI.Text _name = null;
     public UnityEngine.UI.Text Name { get { return _name; } }
+
+    [SerializeField]
+    UnityEngine.UI.Text score = null;
     public UnityEngine.UI.Text Score { get { return score; } }
+
     [SerializeField]
     UnityEngine.UI.Image Background = null;
 
+    /// <summary>
+    /// Sets the value that will be displayed
+    /// </summary>
+    /// <param name="Name">Name of the player that submited the score</param>
+    /// <param name="Score">Score the player got</param>
+    /// <param name="backgroundColor">The color the background will be having</param>
     public void SetValues(string Name, string Score, Color backgroundColor)
     {
         this.score.text = Score;
--- Unity/Assets/Scripts/UI/HighScore/HighScoreMenu.cs
+++ Unity/Assets/Scripts/UI/HighScore/HighScoreMenu.cs
@@ -10,24 +10,30 @@ public class HighScoreMenu : BaseMenu
 
     void Start()
     {
+        HighScoreObjectTemplate.SetActive(false);
+    }
+
+    /// <summary>
+    /// Fills the HighScoreMenu. Requires a template
+    /// </summary>
+    void MakeHighScoreList()
+    {
         GameObject g;
         HighScoreDisplayObject obj;
         bool dark = false;
         for (int i = 0; i < SaveManager.savaData.highScores.Length; i++)
         {
+            //create highscore object
             g = Instantiate(HighScoreObjectTemplate) as GameObject;
             g.SetActive(true);
-            g.transform.SetParent(scoreBoard,false);
+            g.transform.SetParent(scoreBoard, false);
             obj = g.GetComponent<HighScoreDisplayObject>();
 
+            //the the values of the display object
             obj.SetValues(SaveManager.savaData.highScores[i].name, SaveManager.savaData.highScores[i].score.ToString(), dark ? Color.gray : Color.white);
-            obj.Name.color = dark ? (Color.grey + (Color.white / 4f)) : Color.black;
-            obj.Score.color = dark ? (Color.grey + (Color.white / 4f)) : Color.black;
+            obj.Score.color = obj.Name.color = dark ? (Color.grey + (Color.white / 4f)) : Color.black;
             dark = !dark;
         }
-
-        HighScoreObjectTemplate.SetActive(false);
     }
-
 
 }
