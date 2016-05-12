02966129597236ba1fcb489d5383456a840143d5
Nolimet <nolimet@stamdata.nl>
2016-05-12 10:42:06 +0200

    added animation to score indicator making it pop a lil when you gain points. Made score indicator blue

--- Unity/Assets/Scripts/Managers/UIManager.cs
+++ Unity/Assets/Scripts/Managers/UIManager.cs
@@ -11,6 +11,8 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     Text ScoreText = null;
 
+    float ScoreTextScaleMult = 1f;
+
     void Awake()
     {
         GameManager.OnScoreUpdate += GameManager_OnScoreUpdate;
@@ -23,11 +25,29 @@ public class UIManager : MonoBehaviour
         GameManager.OnScoreUpdate -= GameManager_OnScoreUpdate;
     }
 
+    void Update()
+    {
+        if (ScoreTextScaleMult != 1f)
+        {
+            if (ScoreTextScaleMult > 1f)
+            {
+                ScoreText.transform.localScale = Vector3.one * ScoreTextScaleMult;
+                ScoreTextScaleMult -= Time.deltaTime * 2f;
+            }
+            else if (ScoreTextScaleMult < 1f)
+            {
+                ScoreTextScaleMult = 1f;
+                ScoreText.transform.localScale = Vector3.one;
+            }
+        }
+    }
+
     /// <summary>
     /// Called after the score number is changed.
     /// </summary>
     private void GameManager_OnScoreUpdate()
     {
         ScoreText.text = GameManager.Score.ToString();
+        ScoreTextScaleMult = 1.3f;
     }
 }
