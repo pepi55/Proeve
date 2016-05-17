f619d3a76c2c75f5509b2a986eb38291319b38a9
Pepi Dimi <pepi55@live.com>
2016-05-17 08:14:04 +0000

    added new buttons to the main menu

--- HTML5/mainMenu.js
+++ HTML5/mainMenu.js
@@ -1,43 +1,46 @@
 // Main menu state.
+	/** This is a description of the update. */
 var mainMenuState = {
+	currentHighscore: new Array(),
 	preload: function() {
 		// TODO: Add screen resize.
-	  var startButtonImage = game.load.image('button', 'assets/image.png');
+	  game.load.image('playButton', 'assets/buttons/play.png');
+	  game.load.image('highscoreButton', 'assets/buttons/highscore.png');
+	  game.load.image('shopButton', 'assets/buttons/shop.png');
+	  game.load.image('backButton', 'assets/buttons/back.png');
 		game.load.image('invisibleButton', 'assets/invisibleButton.png');
 
-		startButtonImage = startButtonImage.crossOrigin = "Anonymous";
+
 		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	},
 
 	create: function() {
 		game.stage.backgroundColor = '#BBBBBB';
 
+		/**
+		* Represents a book.
+		* @param {string} startButton - this is a button to start the game.
+		*/
 		var startButton;
-		startButton = game.add.button(game.world.centerX, game.world.centerY, 'button', function() {
+		startButton = game.add.button(game.world.centerX, game.world.centerY, 'playButton', function() {
 		    game.state.start('game');
 		}, this);
 		startButton.anchor.setTo(0.5, 0.5);
 
+		/** shop button to start the game. */
 		var shopButton;
-		shopButton = game.add.button(game.world.centerX, game.world.centerY + 100, 'button', function() {
+		shopButton = game.add.button(game.world.centerX, game.world.centerY + 150, 'shopButton', function() {
 			game.state.start('shop');
 		}, this);
 		shopButton.anchor.setTo(0.5, 0.5);
 
-		var optionsButton;
-		optionsButton = game.add.button(game.world.centerX, game.world.centerY + 200, 'button', function() {
-			game.state.start('options');
-		}, this);
-		optionsButton.anchor.setTo(0.5, 0.5);
-
 		var highscoreButton;
-		highscoreButton = game.add.button(game.world.centerX, game.world.centerY + 300, 'button', function() {
+		highscoreButton = game.add.button(game.world.centerX, game.world.centerY + 300, 'highscoreButton', function() {
 			game.state.start('highscore');
 		}, this);
 		highscoreButton.anchor.setTo(0.5, 0.5);
 
-		var resetLocalStorageButton;
-		resetLocalStorageButton = game.add.button(game.world.centerX, game.world.centerY + 500, 'button', function() {
+		var resetLocalStorageButton = game.add.button(game.world.centerX, game.world.centerY + 500, 'button', function() {
 			localStorage.clear();
 		}, this);
 		resetLocalStorageButton.anchor.setTo(0.5, 0.5);
@@ -49,6 +52,10 @@ var mainMenuState = {
 		modeButton.anchor.setTo(0.5, 0.5);
 	},
 
+	/** @method
+	* @name update
+	* @description this is a update function
+	*/
 	update: function() {
 	},
 };
